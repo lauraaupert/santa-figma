97a9301dbeda60abec7b4fed9ebffc1a9e8fb214
Laura Aupert <aupert.laura@gmail.com>
2021-11-06 16:41:52 +0100

    Added swiper for mobile. Converted images to webp. Adjusted styles.

--- figma.js
+++ figma.js
@@ -22,43 +22,108 @@ $(document).ready(function () {
 //     }
 // // 
 
-var title = $(".philcard-title")
-var overlay = $(".overlay")
+// var title = $(".philcard-title")
+// var overlay = $(".overlay")
 
-function appearNature() {
-    $(".nature-title").on("click", function() {
-    $(".nature-text").attr("style", "opacity: 1")
+// function appearNature() {
+//     $(".nature-title").on("click", function() {
+//     $(".nature-text").attr("style", "opacity: 1")
 
-disappearNature()
-})
-}
-function disappearNature() {
-    $(".nature-title").on("click", function() {
-    $(".nature-text").attr("style", "opacity: 0")
+// disappearNature()
+// })
+// }
+// function disappearNature() {
+//     $(".nature-title").on("click", function() {
+//     $(".nature-text").attr("style", "opacity: 0")
 
-appearNature()
-})
-}
-appearNature()
+// appearNature()
+// })
+// }
+// appearNature()
 
-function appearPlay() {
-    $(".play-title").on("click", function() {
-    $(".play-text").attr("style", "opacity: 1")
+// function appearPlay() {
+//     $(".play-title").on("click", function() {
+//     $(".play-text").attr("style", "opacity: 1")
 
-disappearPlay()
-})
-}
-function disappearPlay() {
-    $(".play-title").on("click", function() {
-    $(".play-text").attr("style", "opacity: 0")
+// disappearPlay()
+// })
+// }
+// function disappearPlay() {
+//     $(".play-title").on("click", function() {
+//     $(".play-text").attr("style", "opacity: 0")
 
-appearPlay()
-})
-}
-appearPlay()
+// appearPlay()
+// })
+// }
+// appearPlay()
 
 
+var text = new Swiper('.acc-text', {
+    slidesPerView: 'auto',
+    // autoHeight: true,
+    spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    mousewheel: true,
+    // {
+    //     forceToAxis: true,
+    
+    //   },
+      keyboard: {
+        enabled: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+      },
+      slideShadow: true
+  })
 
+//   var swiper = new Swiper('.acc-text', {
+//     effect: 'coverflow',
+//     centeredSlides: true,
+//     mousewheel: {
+//       forceToAxis: true,
+  
+//     },
+//     keyboard: {
+//       enabled: true,
+//     },
+//     slidesPerView: 'auto',
+//     loop: true,
+//     coverflowEffect: {
+//       rotate: 40,
+//       stretch: -140,
+//       depth: 500,
+//       modifier: 1,
+  
+//       slideShadows: true,
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//        // Navigation arrows
+//        navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//       controller: {
+//         by: 'slide',
+//         // control: text,
+//         inverse: false,
+//       },
+//       // thumbs: {
+//       //   swiper: text,
+//       //   autoScrollOffset: 1,
+//       // }
+//       // thumbs: {
+//       //   swiper: galleryThumbs,
+//       // },
+  
+//   });
+  
 
 
 
